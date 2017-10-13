37e0c9230720876ba4413ec078be20178b11dc97
Thang Tran <thangtq@10.0.2.15>
2017-10-13 08:23:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-ROOT=/home/thangtq/Work/HDH_Project
+# ROOT=/home/thangtq/Work/HDH_Project
+ROOT=$PWD
+LIBSRC=$ROOT/lib
+
 TARGET=decstation-ultrix
 HOST=i686-pc-linux-gnu
 PREFIX=$ROOT/build/cross
-GCCDIR=$ROOT/lib/gcc-2.95.3
-BINDIR=$ROOT/lib/binutils-2.11.2
+# GCCDIR=lib/gcc-2.95.3
+# BINDIR=lib/binutils-2.11.2
 OBJGCC=$ROOT/build/gcc-obj
 OBJBIN=$ROOT/build/bin-obj
+
+cd $LIBSRC
+tar -xzvf binutils-2.11.2.tar.gz
+tar -xzvf gcc-2.95.3.tar.gz
 
 mkdir $ROOT/build
 mkdir $OBJGCC
@@ -15,7 +22,7 @@ mkdir $OBJBIN
 mkdir $PREFIX
 
 cd $OBJBIN
-$BINDIR/configure --host=$HOST --target=$TARGET --prefix=$PREFIX -v
+$LIBSRC/binutils-2.11.2/configure --host=$HOST --target=$TARGET --prefix=$PREFIX -v
 # $BINDIR/make all install CC=gcc34
 make CC=gcc34
 make install
@@ -23,7 +30,7 @@ make install
 mkdir $PREFIX/decstation-ultrix/sys-include
 
 cd $OBJGCC
-$GCCDIR/configure --host=$HOST \
+$LIBSRC/gcc-2.95.3/configure --host=$HOST \
     --target=$TARGET \
     --prefix=$PREFIX \
     --program-prefix=cross \

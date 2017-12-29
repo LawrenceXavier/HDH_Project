70c4c2170287448a2c05d5106c53b1a742bc9c9c
LawrenceXavier <lawrencealgorithm@gmail.com>
2017-12-29 08:41:26 +0700

    Add class PTable

---
+++ src/nachos-3.4/code/threads/ptable.cc
@@ -0,0 +1,29 @@
+#include "ptable.h"
+
+int PTable::ExecUpdate(char* name) {
+
+}
+
+int PTable::ExitUpdate(int ec) {
+
+}
+
+int PTable::JoinUpdate(int id) {
+
+}
+
+int PTable::GetFreeSlot() {
+
+}
+
+bool PTable::IsExist(int pid) {
+
+}
+
+void PTable::Remove(int pid) {
+
+}
+
+char* PTable::GetFileName(int id) {
+
+}
